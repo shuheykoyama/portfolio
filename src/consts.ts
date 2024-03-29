ac11218971f286c9f22bfb6607433154ce19dd32
shuheykoyama <shuheykoyama@gmail.com>
2024-03-29 23:46:43 +0900

    feat: 🎸 update header
    Signed-off-by: shuheykoyama <shuheykoyama@gmail.com>

--- src/consts.ts
+++ src/consts.ts
@@ -4,3 +4,80 @@
 export const SITE_TITLE = 'Shuhey Koyama'
 export const SITE_DESCRIPTION = "Welcome to Shuhey Koyama's Portfolio Website!"
 export const HOMEPAGE_URL = 'https://shuheykoyama.com'
+
+// import type { Site, Page, Links, Socials } from '@types'
+//
+// // Global
+// export const SITE: Site = {
+//   TITLE: 'Shuhey Koyama',
+//   DESCRIPTION: "Welcome to Shuhey Koyama's Portfolio Website!",
+//   AUTHOR: 'Shuhey Koyama'
+// }
+//
+// // Work Page
+// export const WORKS: Page = {
+//   TITLE: 'Works',
+//   DESCRIPTION: 'The works I have created so far.'
+// }
+//
+// // Search Page
+// export const SEARCH: Page = {
+//   TITLE: 'Search',
+//   DESCRIPTION: 'Search all works by keyword.'
+// }
+//
+// // Links
+// export const LINKS: Links = [
+//   {
+//     TEXT: 'Home',
+//     HREF: '/'
+//   },
+//   {
+//     TEXT: 'Works',
+//     HREF: '/works'
+//   },
+//   {
+//     TEXT: 'Source',
+//     HREF: 'https://github.com/shuheykoyama/portfolio'
+//   }
+// ]
+//
+// // Socials
+// export const SOCIALS: Socials = [
+//   {
+//     NAME: 'Email',
+//     ICON: 'email',
+//     TEXT: 'shuheykoyama@gmail.com',
+//     HREF: 'mailto:shuheykoyama@gmail.com'
+//   },
+//   {
+//     NAME: 'GitHub',
+//     ICON: 'github',
+//     TEXT: 'shuheykoyama',
+//     HREF: 'https://github.com/shuheykoyama'
+//   },
+//   {
+//     NAME: 'X',
+//     ICON: 'twitter-x',
+//     TEXT: 'shuheykoyama',
+//     HREF: 'https://x.com/shuheykoyama'
+//   },
+//   {
+//     NAME: 'Facebook',
+//     ICON: 'facebook',
+//     TEXT: 'shuheykoyama',
+//     HREF: 'https://www.facebook.com/shuheykoyama'
+//   },
+//   {
+//     NAME: 'Instagram',
+//     ICON: 'instagram',
+//     TEXT: 'shuheykoyama',
+//     HREF: 'shuheykoyama@gmail.com'
+//   },
+//   {
+//     NAME: 'YouTube',
+//     ICON: 'email',
+//     TEXT: 'shuheykoyama@gmail.com',
+//     HREF: 'https://www.youtube.com/channel/UCzPjLzS8qL26bRrBDp4iPyw'
+//   }
+// ]
