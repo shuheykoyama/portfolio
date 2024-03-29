36874bd2183b28cf6976a04d68d05ba06639c543
shuheykoyama <shuheykoyama@gmail.com>
2024-03-29 23:45:54 +0900

    feat: 🎸 configure settings for deployment
    Signed-off-by: shuheykoyama <shuheykoyama@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import addClasses from 'rehype-add-classes'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://uses.craftz.dog/',
+  site: 'https://shuheykoyama.com/',
   integrations: [
     sitemap(),
     react(),
