a3f1950de2627fb808f45d6c6744ca7bc07cb192
shuheykoyama <shuheykoyama@gmail.com>
2024-12-19 04:30:37 +0900

    build: add React integration to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://shuheykoyama.com/',
-  integrations: [tailwind()],
+  integrations: [react(), tailwind()],
 });
