2681253646da1e09959eda9788206a698fe6d9a3
shuheykoyama <shuheykoyama@gmail.com>
2024-12-20 17:30:00 +0900

    fix: update site URL and add base path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shuheykoyama.com/',
+  site: 'https://shuheykoyama.github.io',
+  base: '/portfolio',
   integrations: [react(), tailwind()],
 });
