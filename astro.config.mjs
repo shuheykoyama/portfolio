906c6fb9ae8e3094e3e9525ac32273a169c8efe2
shuheykoyama <shuheykoyama@gmail.com>
2024-12-24 13:39:30 +0900

    fix: update base URLs and paths to remove "/portfolio" prefix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://shuheykoyama.github.io',
-  base: '/portfolio/',
+  site: 'https://shuheykoyama.com/',
   integrations: [react(), tailwind()],
 });
